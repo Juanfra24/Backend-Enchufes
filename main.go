513927b67705c3eee55883293851e371c8dd3632
Juanfra24 <jternicien@alumnos.uai.cl>
2018-09-23 21:50:29 -0300

    Fin

--- main.go
+++ main.go
@@ -1,28 +1,18 @@
 package main
-
 import (
 	"net/http"
 	"os"
 )
-
 func main() {
 	fileServer := http.FileServer(http.Dir("App"))
 	http.Handle("/App/", http.StripPrefix("/App/", fileServer))
 
+	
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "8000"
 	}
 
-	http.ListenAndServe(":8000", nil)
-
+	http.ListenAndServe(":"+port, nil)
 }
-
-/*
-func home(w http.Response, r *http.Request) {
-	var b bytes.Buffer // A Buffer needs no initialization.
-	b.Write([]byte("Hello World"))
-
-}
-*/
