e908372ffe2100392b0aedccbe4a0641c2ded1c4
Juanfra24 <jternicien@alumnos.uai.cl>
2018-09-24 01:38:58 -0300

    DB2

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"os"
-	"database/sql"
-    _ "github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -21,29 +22,29 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
+
+	//DB
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	/*
-		//DB
-		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+		_, err = db.Exec("CREATE DATABASE " + "Encuhfe")
 		if err != nil {
 			panic(err)
 		}
-		defer db.Close()
 
+		_, err = db.Exec("USE " + "Enchufe")
+		if err != nil {
+			panic(err)
+		}
 
-			_, err = db.Exec("CREATE DATABASE " + "Encuhfe")
-			if err != nil {
-				panic(err)
-			}
-
-			_, err = db.Exec("USE " + "Enchufe")
-			if err != nil {
-				panic(err)
-			}
-
-			_, err = db.Exec("CREATE TABLE example ( id integer, data varchar(32) )")
-			if err != nil {
-				panic(err)
-			}
+		_, err = db.Exec("CREATE TABLE example ( id integer, data varchar(32) )")
+		if err != nil {
+			panic(err)
+		}
 	*/
 	//Host pagina
 	http.ListenAndServe(":"+port, nil)
